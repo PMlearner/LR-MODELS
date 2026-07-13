6f5ee4acc0844f061b2e6fc47ea1258497ecf745
PMlearner <ponnusamy4121999@gmail.com>
2026-07-13 12:48:31 +0530

    learning

--- test.py
+++ test.py
@@ -1,44 +1,58 @@
 
-def bubbleSort(array):
+# def bubbleSort(array):
     
-  # loop to access each array element
-  for i in range(len(array)):
+#   # loop to access each array element
+#   for i in range(len(array)):
 
-    # loop to compare array elements
-    for j in range(0, len(array) - i):
+#     # loop to compare array elements
+#     for j in range(0, len(array) - i):
 
-      # compare two adjacent elements
-      # change > to < to sort in descending order
-      if array[j] > array[j + 1]:
+#       # compare two adjacent elements
+#       # change > to < to sort in descending order
+#       if array[j] > array[j + 1]:
 
-        # swapping elements if elements
-        # are not in the intended order
-        temp = array[j]
-        array[j] = array[j+1]
-        array[j+1] = temp
+#         # swapping elements if elements
+#         # are not in the intended order
+#         temp = array[j]
+#         array[j] = array[j+1]
+#         array[j+1] = temp
 
 
-data = [-2, 45, 0, 11, -9]
+# data = [-2, 45, 0, 11, -9]
 
-# bubbleSort(data)
+# # bubbleSort(data)
 
-# print('Sorted Array in Ascending Order:')
-# print(data)
+# # print('Sorted Array in Ascending Order:')
+# # print(data)
 
-'''Question:Partition the labels in the given string '''
-Input=  "ababcbacadefegdehijhklij"
-Output= [9, 7, 8]
-def partion_logic(st):
-  last_index={ch:i for i,ch in enumerate(st)}
-  start,end=0,0
-  part=[]
-  for i,ch in enumerate(st):
-    end=max(end,last_index[ch])
-    if i==end:
-      part.append(end-start+1)
-      start=i+1
-  print(part)
+# '''Question:Partition the labels in the given string '''
+# Input=  "ababcbacadefegdehijhklij"
+# Output= [9, 7, 8]
+# def partion_logic(st):
+#   last_index={ch:i for i,ch in enumerate(st)}
+#   start,end=0,0
+#   part=[]
+#   for i,ch in enumerate(st):
+#     end=max(end,last_index[ch])
+#     if i==end:
+#       part.append(end-start+1)
+#       start=i+1
+#   print(part)
 
-partion_logic(Input)
+# partion_logic(Input)
 
     
+# nums = [2, 7, 11, 15]
+# target = 9
+# values=[]
+# for i in range(len(nums)):
+#     for j in range(i+1,len(nums)):
+#         if nums[i]+nums[j]==target:
+#             values.append((i,j))
+# print(values)
+
+d = {"a": 1, "b": 2}
+
+del d["a"]      # use when key exists
+# value = d.pop("b")  # use when you need the removed value
+print(d)
